41514dbad8492bc0f33bba3067d3d993f3d5d01b
SakipGurler <skpgrlr@gmail.com>
2023-08-13 21:50:54 +0300

    guessButton add

--- index.js
+++ index.js
@@ -2,9 +2,61 @@ let randomNumber = parseInt(Math.random() * 101);
 const guessButton = document.getElementById("guessButton");
 const guessInput = document.getElementById("guessInput");
 const resultDiv = document.getElementById("resultDiv");
-const choices = document.getElementsByClassName("choices")
+const choices = document.querySelector(".choices")
 
 let count = 5;
 
 let maxGuess = 0;
 
+choices.textContent = count
+
+guessButton.addEventListener("click", function () {
+    console.log(randomNumber);
+  if (count <= maxGuess) {
+    resultDiv.textContent = `You Lose ! The correct number was ${randomNumber} :(`;
+    guessButton.disabled = true;
+    setTimeout(function() {
+        location.reload();
+    }, 5000);
+    return;
+  }
+
+  const guess = Number(guessInput.value);
+
+  if (guess > 100 || guess < 0) {
+    resultDiv.textContent = `Please Enter Valid Number Between 1-100`;
+  } else {
+    count--;
+    choices.textContent = count
+    
+
+    if (guess === randomNumber) {
+      resultDiv.textContent = `Congratulations! You Win ♥`;
+      guessButton.disabled = true;
+      setTimeout(function() {
+        location.reload();
+    }, 5000);
+      
+    } else if (guess < randomNumber) {
+      resultDiv.textContent = `Too Low! Try Again`;
+    } else {
+      resultDiv.textContent = `Too High! Try Again`;
+    }
+  }
+
+  guessInput.value = "";
+  guessInput.focus()
+
+  
+});
+
+guessInput.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        guessButton.click();
+    }
+});
+
+
+window.addEventListener('load', () => {
+    guessInput.focus();
+});
